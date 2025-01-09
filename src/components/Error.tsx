81b4c6e097713cdc1f2c275bb13cf660500b7534
VICTOR HUGO SOTO GANDARA <212310861@lerdo.tecnm.mx>
2025-01-09 16:45:02 -0600

    changes in buttons

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error({children} : {children: React.ReactNode}) {
   return (
     <p 
-        className="bg-red-200 text-red-600 border-2 border-red-600 text-center font-medium rounded-md mt-2"
+        className="bg-red-100 text-red-600 border-2 border-red-600 text-center font-medium rounded-md mt-2"
     >
         {children}
     </p>
